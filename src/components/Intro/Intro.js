c01e7d41a33b290e533433869ad4a207f4648ef5
Marcin Winiarski <winiardesign@gmail.com>
2022-09-12 20:03:52 +0200

    hero section

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import FloatingDiv from '../FloatingDiv/FloatingDvi';
 import './Intro.css';
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+import thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import glassesimoji from '../../img/glassesimoji.png';
 
 function Intro() {
   return (
@@ -21,7 +28,25 @@ function Intro() {
             </div>
         </div>
         <div className="i-right">
-            i am right side
+            <img src={Vector1} alt="" />
+            <img src={Vector2} alt="" />
+            <img src={boy} alt="" />
+            <img src={glassesimoji} alt="" />
+            <div style={{top: '-4%', left: '68%'}}>
+                <FloatingDiv image={Crown} txt1='Web' txt2="Developer" />
+            </div>
+            <div style={{top: '18rem', left: '0rem'}}>
+            <FloatingDiv image={thumbup} txt1='Best Design' txt2="Award" />
+            </div>
+            {/* blur divs*/}
+            <div className="blur" style={{background: "rgb(238 210 255)"}}></div>
+            <div className='blur' style={ {
+                background: '#c1f5ff',
+                top: '17rem',
+                width: '21rem',
+                height: '11rem',
+                left: '-9rem'
+            }}></div>
         </div>
     </div>
   )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
                     <li>Testimonials</li>
                 </ul>
             </div>
-            <button className="button n-button">Contact us</button>
+            <button className="button n-button">Contact</button>
         </div>
     </div>
   )
